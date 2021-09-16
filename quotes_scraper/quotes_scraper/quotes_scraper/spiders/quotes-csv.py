d50f976be3756a482a52bd842bb151a8ccd18f99
Oscar Bazañez <oscar-bazanez@hotmail.com>
2021-09-15 23:47:16 -0500

    Spidet Quotes generar JSON y CSV

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes-csv.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes-csv.py
@@ -1,16 +1,16 @@
 import scrapy
 
 class QuotesSpider(scrapy.Spider):
-    name = 'quotes-json'
+    name = 'quotes-csv'
     start_urls = [
         'http://quotes.toscrape.com/'
     ]
     custom_settings = {
         'FEEDS': {
-            'quotes.json': {
-                'format': 'json',
+            'quotes.csv': {
+                'format': 'csv',
                 'encoding': 'utf8',
-                'fields': ['title', 'top_tags', 'quotes','authors'],
+                'fields': ['title', 'top_tags','quotes','authors','tags'],
                 'overwrite': True
             }
         },
@@ -32,39 +32,38 @@ class QuotesSpider(scrapy.Spider):
 
 
     def parse_only_quotes(self, response, **kwargs):
-        current_authors = response.xpath(self.__authors).getall()
-        current_quotes = response.xpath(self.__quotes).getall()
-        current_quotes_tags = response.xpath(self.__quotes_tags).getall()
-        
+        authors = response.xpath(self.__authors).getall()
+        quotes = response.xpath(self.__quotes).getall()
+        quotes_tags = response.xpath(self.__quotes_tags).getall()
+        next_page_buttom_link = response.xpath(self.__next_page_buttom_link).get()
+
         if kwargs:
             quotes = kwargs['quotes']
 
-        quotes = self.get_full_quotes(quotes, current_quotes, current_authors,current_quotes_tags)
+        # Guardando las citas, tags y el autor
+        for quote, author, tags in zip(quotes, authors, quotes_tags):
+            yield {
+            'quotes': quote,
+            'authors': author,
+            'tags': tags,
+            }
 
-        next_page_buttom_link = response.xpath(self.__next_page_buttom_link).get()
         if next_page_buttom_link:
             yield response.follow(
                 next_page_buttom_link, 
                 callback=self.parse_only_quotes,
                 cb_kwargs={'quotes': quotes}
             )
-        else:
-            # Guardando las citas y el autor
-            yield {
-                'quotes': quotes,
-            }
 
 
 
     def parse(self, response):
         title = response.xpath(self.__title).get()
-        current_authors = response.xpath(self.__authors).getall()
-        current_quotes = response.xpath(self.__quotes).getall()
-        current_quotes_tags = response.xpath(self.__quotes_tags).getall()
+        authors = response.xpath(self.__authors).getall()
+        quotes = response.xpath(self.__quotes).getall()
+        quotes_tags = response.xpath(self.__quotes_tags).getall()
         top_tags = response.xpath(self.__top_tags).getall()
-
-        quotes = []
-        quotes = self.get_full_quotes([], current_quotes, current_authors,current_quotes_tags)
+        next_page_buttom_link = response.xpath(self.__next_page_buttom_link).get()
 
         # Obtener la cantidad de top deseado
         top = getattr(self, 'top', None)
@@ -72,28 +71,21 @@ class QuotesSpider(scrapy.Spider):
             top = int(top)
             top_tags = top_tags[:top]
 
-
         yield {
             'title': title,
             'top_tags': top_tags
         }
 
-        next_page_buttom_link = response.xpath(self.__next_page_buttom_link).get()
+        for quote, author, tags in zip(quotes, authors, quotes_tags):
+            yield {
+            'quotes': quote,
+            'authors': author,
+            'tags': tags,
+            }
+        
         if next_page_buttom_link:
             yield response.follow(
                 next_page_buttom_link, 
                 callback=self.parse_only_quotes,
                 cb_kwargs={'quotes': quotes} # Pasamos argumentos a la otra funcion
             )
-            
-
-    def get_full_quotes(self, quotes, current_quotes, current_authors,current_quotes_tags):
-        if len(current_quotes) == len(current_authors):
-            for quote, author, tags in zip(current_quotes, current_authors,current_quotes_tags):
-                full_quote = {
-                'quote': quote,
-                'author': author,
-                'tags': tags,
-                }
-                quotes.append(full_quote)
-        return quotes
