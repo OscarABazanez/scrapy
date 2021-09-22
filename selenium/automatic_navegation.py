bacff746f0e7a3a0d88a1aecc56a168f3c5d3771
Oscar Bazañez <oscar-bazanez@hotmail.com>
2021-09-22 12:49:53 -0500

    Selenium Automatizar navegacion

---
+++ selenium/automatic_navegation.py
@@ -0,0 +1,43 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+
+class CompareProductTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        cls.driver = webdriver.Chrome(executable_path = '/usr/bin/chromedriver' , options=options)
+        driver = cls.driver
+        driver.get('https://google.com/')
+        driver.maximize_window()
+        # driver.implicitly_wait(10)
+
+
+    def test_browser_navigation(self):
+        driver = self.driver
+
+        search_field = driver.find_element_by_name('q')
+        search_field.clear()
+        search_field.send_keys('platzi')
+        search_field.submit()
+
+        driver.back() # Retroceder navegador
+        driver.forward() # Avanzar
+        driver.refresh() # Actualizar página
+ 
+        
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+if __name__ == '__main__':
+  # output es el nombre del reporte
+  unittest.main(verbosity=2 , testRunner= HTMLTestRunner(output = 'reportes', report_name='register-new-user-report'))
